e70802e2f4be3b28043d0170d953180d0ec34d64
Jerrod Varney <jerrodvarney@gmail.com>
2023-09-18 23:13:29 -0700

    added hover highlight to cards and adjusted some spacing for better readability

--- src/components/ExpCard.jsx
+++ src/components/ExpCard.jsx
@@ -5,19 +5,18 @@ import TechChips from './TechChips.jsx';
 export default function ExpCard({ exp }) {
   return (
     <a className="info-card-link" href={exp.companySite} target="_blank" rel="noreferrer">
-      <div className="info-card-highlight-box">
-        <div className="info-card">
-          <div className="info-card-left exp-date-container">
-            <span className="exp-dates">{`${exp.startDate} — ${exp.endDate}`}</span>
-          </div>
-          <div className="info-card-main">
-            <h3>
-              {`${exp.title} · ${exp.company}`}
-              <FiArrowUpRight className="info-card-title-icon" />
-            </h3>
-            <p>{exp.description}</p>
-            <TechChips tech={exp.technologies} />
-          </div>
+      <div className="info-card-lowlight-box" />
+      <div className="info-card">
+        <div className="info-card-left exp-date-container">
+          <span className="exp-dates">{`${exp.startDate} — ${exp.endDate}`}</span>
+        </div>
+        <div className="info-card-main">
+          <h3>
+            {`${exp.title} · ${exp.company}`}
+            <FiArrowUpRight className="info-card-title-icon" />
+          </h3>
+          <p>{exp.description}</p>
+          <TechChips tech={exp.technologies} />
         </div>
       </div>
     </a>
