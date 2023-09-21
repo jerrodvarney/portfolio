cc13f1ec577eb115919af3b15a13e452108907c8
Jerrod Varney <jerrodvarney@gmail.com>
2023-09-20 22:10:51 -0700

    key error fix

--- src/components/TechChips.jsx
+++ src/components/TechChips.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 export default function TechChips({ tech }) {
+  let counter = 1;
   return (
     <div className="tech-chip-container">
-      {tech.map((t) => <span className="tech-chip">{t}</span>)}
+      {tech.map((t) => <span className="tech-chip" key={counter++}>{t}</span>)}
     </div>
   );
 }
